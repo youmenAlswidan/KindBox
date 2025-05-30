03501d442b19bf17e7825c1e34fd982d1a1160cb
dareen <dareenalideeb653@gmail.com>
2025-05-30 15:43:53 +0300

    Remover old API

--- routes/api.php
+++ routes/api.php
@@ -1,26 +1,11 @@
 <?php
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\RoleController;
-use App\Http\Controllers\UserController;
-use App\Http\Controllers\CategoryController;
-use App\Http\Controllers\ShopController;
-use App\Http\Controllers\ProductController;
-use App\Http\Controllers\DiscountController;
-use App\Http\Controllers\WishlistController;
-use App\Http\Controllers\NotificationController;
-use App\Http\Controllers\GroupOrderController;
-use App\Http\Controllers\GroupOrderMemberController;
-use App\Http\Controllers\OrderController;
-use App\Http\Controllers\OrderItemController;
-use App\Http\Controllers\PaymentController;
-use App\Http\Controllers\ShopDocumentController;
-use App\Http\Controllers\VehicleController;
-use App\Http\Controllers\DeliveryController;
+
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\Driver\DeliveryController;
+
 
 
 /*
@@ -34,30 +19,20 @@ use App\Http\Controllers\AuthController;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
 
 
-Route::apiResource('roles', RoleController::class);
-Route::apiResource('users', UserController::class);
-Route::apiResource('categories', CategoryController::class);
-Route::apiResource('shops', ShopController::class);
-Route::apiResource('products', ProductController::class);
-Route::apiResource('discounts', DiscountController::class);
-Route::apiResource('wishlists', WishlistController::class);
-Route::apiResource('notifications', NotificationController::class);
-Route::apiResource('group-orders', GroupOrderController::class);
-Route::apiResource('group-order-members', GroupOrderMemberController::class);
-Route::apiResource('orders', OrderController::class);
-Route::apiResource('order-items', OrderItemController::class);
-Route::apiResource('payments', PaymentController::class);
-Route::apiResource('shop-documents', ShopDocumentController::class);
-Route::apiResource('vehicles', VehicleController::class);
-Route::apiResource('deliveries', DeliveryController::class);
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+
+
+
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
 Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
+
+//Driver 
+Route::middleware(['auth:sanctum', 'role:driver'])->prefix('driver')->group(function () {
+    Route::get('/deliveries', [DeliveryController::class, 'index']);
+    Route::post('/deliveries/{id}/status', [DeliveryController::class, 'updateStatus']);
+});
+
+
+
